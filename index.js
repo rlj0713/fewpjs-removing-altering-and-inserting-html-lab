73e3c4b7c3424418ba063a0e5dd94e39b7c599ce
Reid <rlj0713@gmail.com>
2021-07-14 15:18:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "Reid is the champion"
+newHeader.className = "victory"
